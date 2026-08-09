7a9af12b685a6a87a6b0d01543b54ca00fcae56e
Dhiraj Rathore <33519880+drj477@users.noreply.github.com>
2026-08-10 01:23:16 +0530

    Add mobile Virtual Try-On repository

---
+++ mobile/lib/features/outfits/data/virtual_try_on_repository.dart
@@ -0,0 +1,27 @@
+import 'package:mobile/features/outfits/data/models/virtual_try_on_models.dart';
+import 'package:mobile/features/outfits/data/virtual_try_on_api.dart';
+
+class VirtualTryOnRepository {
+  VirtualTryOnRepository(this._api);
+
+  final VirtualTryOnApi _api;
+
+  Future<VirtualTryOnResult> generate({
+    required String profileId,
+    required VirtualTryOnRequest request,
+  }) {
+    return _api.generate(profileId: profileId, request: request);
+  }
+
+  Future<VirtualTryOnSaveResult> setSaved({
+    required String profileId,
+    required String resultId,
+    required bool saved,
+  }) {
+    return _api.setSaved(
+      profileId: profileId,
+      resultId: resultId,
+      saved: saved,
+    );
+  }
+}
