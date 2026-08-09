013869ab22dc976f81900d93c41b5a8ae13983f0
Dhiraj Rathore <33519880+drj477@users.noreply.github.com>
2026-08-10 01:23:05 +0530

    Add mobile Virtual Try-On API client

---
+++ mobile/lib/features/outfits/data/virtual_try_on_api.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+
+import 'package:mobile/core/network/api_client.dart';
+import 'package:mobile/core/network/api_endpoints.dart';
+import 'package:mobile/features/outfits/data/models/virtual_try_on_models.dart';
+
+class VirtualTryOnApi {
+  VirtualTryOnApi(this._apiClient);
+
+  final ApiClient _apiClient;
+
+  Future<VirtualTryOnResult> generate({
+    required String profileId,
+    required VirtualTryOnRequest request,
+  }) async {
+    final response = await _apiClient.dio.post(
+      '${ApiEndpoints.profiles}/$profileId/try-on',
+      data: request.toJson(),
+      options: Options(
+        receiveTimeout: const Duration(seconds: 100),
+        sendTimeout: const Duration(seconds: 20),
+      ),
+    );
+
+    return VirtualTryOnResult.fromJson(
+      response.data as Map<String, dynamic>,
+    );
+  }
+
+  Future<VirtualTryOnSaveResult> setSaved({
+    required String profileId,
+    required String resultId,
+    required bool saved,
+  }) async {
+    final response = await _apiClient.dio.patch(
+      '${ApiEndpoints.profiles}/$profileId/try-on/$resultId/save',
+      queryParameters: {'saved': saved},
+    );
+
+    return VirtualTryOnSaveResult.fromJson(
+      response.data as Map<String, dynamic>,
+    );
+  }
+}
