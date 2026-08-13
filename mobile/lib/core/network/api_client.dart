9076187a721a989d122e6bcad324cf0649491795
Dhiraj Rathore <33519880+drj477@users.noreply.github.com>
2026-08-13 18:25:21 +0530

    fix: allow extended timeout for image processing uploads

--- mobile/lib/core/network/api_client.dart
+++ mobile/lib/core/network/api_client.dart
@@ -12,34 +12,58 @@ class ApiClient {
   Future<Response<dynamic>> get(
     String path, {
     Map<String, dynamic>? queryParameters,
+    Options? options,
   }) {
-    return dio.get(path, queryParameters: queryParameters);
+    return dio.get(
+      path,
+      queryParameters: queryParameters,
+      options: options,
+    );
   }
 
   Future<Response<dynamic>> post(
     String path, {
     dynamic data,
     Map<String, dynamic>? queryParameters,
+    Options? options,
   }) {
-    return dio.post(path, data: data, queryParameters: queryParameters);
+    return dio.post(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
   }
 
   Future<Response<dynamic>> postMultipart(
     String path, {
     required FormData data,
+    Options? options,
   }) {
-    return dio.post(path, data: data);
+    return dio.post(path, data: data, options: options);
   }
 
-  Future<Response<dynamic>> put(String path, {dynamic data}) {
-    return dio.put(path, data: data);
+  Future<Response<dynamic>> put(
+    String path, {
+    dynamic data,
+    Options? options,
+  }) {
+    return dio.put(path, data: data, options: options);
   }
 
-  Future<Response<dynamic>> patch(String path, {dynamic data}) {
-    return dio.patch(path, data: data);
+  Future<Response<dynamic>> patch(
+    String path, {
+    dynamic data,
+    Options? options,
+  }) {
+    return dio.patch(path, data: data, options: options);
   }
 
-  Future<Response<dynamic>> delete(String path, {dynamic data}) {
-    return dio.delete(path, data: data);
+  Future<Response<dynamic>> delete(
+    String path, {
+    dynamic data,
+    Options? options,
+  }) {
+    return dio.delete(path, data: data, options: options);
   }
 }
