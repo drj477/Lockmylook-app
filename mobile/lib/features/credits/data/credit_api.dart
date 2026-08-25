18fb9767a17aac96b9932cd9ee24dc6dff525c7b
Dhiraj Rathore <33519880+drj477@users.noreply.github.com>
2026-08-25 05:39:53 +0530

    feat(credits): add mobile credit balance API

---
+++ mobile/lib/features/credits/data/credit_api.dart
@@ -0,0 +1,15 @@
+import 'package:mobile/core/network/api_client.dart';
+import 'package:mobile/core/network/api_endpoints.dart';
+import 'package:mobile/features/credits/data/models/credit_models.dart';
+
+class CreditApi {
+  CreditApi(this._apiClient);
+
+  final ApiClient _apiClient;
+
+  Future<CreditBalance> getBalance() async {
+    final response = await _apiClient.get(ApiEndpoints.creditsBalance);
+    final data = response.data as Map<String, dynamic>;
+    return CreditBalance.fromJson(data['data'] as Map<String, dynamic>);
+  }
+}
